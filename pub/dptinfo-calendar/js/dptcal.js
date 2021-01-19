860e25165015b294ad1b144a22364c5486f559d5
Yann Duplouy <duplouy@lsv.fr>
2021-01-19 15:04:58 +0100

    Adding necessary functions

--- pub/dptinfo-calendar/js/dptcal.js
+++ pub/dptinfo-calendar/js/dptcal.js
@@ -18,7 +18,9 @@ function DptinfoCalendarReady (callback) {
 	}
 }
 
-/* Now, I reuse code that was used for the computer science department of ENS Paris-Saclay website */
+/* Now, I reuse code that was used for the computer science department of ENS Paris-Saclay website
+ * More specifically, the code reused is from http://dptinfo.ens-cachan.fr/dptCalendar/dptCalendar.js?2
+ */
 
 var dptinfoCalDefaultOptions = {
 	defaultView : 'agendaWeek',
@@ -42,18 +44,67 @@ function newEvent (when)
 	return { start: when[0], end: when[1], tips: [] };
 }
 
+function setEventProperty (cours, event, key, value)
+{
+	var ck = "ev_"+key;
+	if (typeof event[ck] == 'undefined') event[ck]=value;
+	if (typeof cours[key] != 'undefined') event[ck]=cours[key];
+}
+
+function getEventProperty (event, key)
+{
+	var ck = "ev_"+key;
+	if (typeof event[ck] == 'string') return event[ck];
+	if (typeof event[ck] != 'object') return "";
+	return event[ck].join("\n");
+}
+
+function applySettings(obj, event)
+{
+	if (typeof obj.tag == 'string') event.tag=obj.tag;
+	if (typeof obj.vue == 'string') event.vue = obj.vue;
+	if (typeof obj.title == 'string') event.title = obj.title;
+	if (typeof obj.color == 'string') event.color = obj.color;
+	if (typeof obj.textColor == 'string')
+		event.textColor = obj.textColor;
+	if (typeof obj.borderColor == 'string')
+		event.borderColor = obj.borderColor;
+	if (typeof obj.properties == 'object')
+		jQuery.extend(event,obj.properties);
+
+	var linktext = "Plus d'information";
+	if (typeof obj.info == 'string') event.tips.push(
+		{ icon: 'info', text: obj.info });
+	if (typeof obj.url == 'string') event.tips.push(
+		{ icon: 'link', text: linktext, url: obj.url });
+	if (typeof obj.url == 'object')
+		for (var k in obj.url)
+			event.tips.push({icon:'link', text:k, url:obj.url[k]});
+}
+
 function finalizeEvent (event)
 {
 //	even.tip = makeTips(event.tips);	// ToDo: See if makeTips is necessary and implement it if needed
 	calEvents.push(event);
 }
 
+/**************************************************************************************************************************/
+
 function calendarEvent (data, cev)
 {
 	var event = newEvent([cev.date+"T"+cev.start, cev.date+"T"+cev.end]);
-	// incomplete
+
+	setEventProperty(cev, event, "speaker", "");
+	setEventProperty(cev, event, "name", "");
+	setEventProperty(cev, event, "room", "");
+
+	event.title = event.ev_name + "\n" + event.ev_room + "\n" + getEventProperty(event, "speaker");
+
+	applySettings(cev, event);
 	finalizeEvent(event);
 }
+
+/**************************************************************************************************************************/
 
 function genCalendar(style,name,callback)
 {
